c308e74f7898c56d68a94140f8571bf2a16e9536
Md Aminul Islam <mr.aminul118@gmail.com>
2025-01-01 23:52:21 +0600

    Delete cart item by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -81,6 +81,12 @@ async function run() {
       const email = req.query.email;
       const query = { email: email };
       const result = await cartCollections.find(query).toArray();
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollections.deleteOne(query);
       res.send(result);
     });
 
